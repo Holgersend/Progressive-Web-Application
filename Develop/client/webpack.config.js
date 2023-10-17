7924452219f00a16898acd69579c4e49eb18b471
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-10-17 18:27:43 -0500

    Update webpack.config.js
    added workbox plugins

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -11,19 +11,49 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      header: './src/js/header.js',
+      editor: './src/editor.js',
+      database: './src/editor.js'
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: './src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Just another text editor',
+        background_color: '#447381',
+        theme_color: '#C6C8C8',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'), 
+          },
+        ],
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          
+        }
       ],
     },
   };
