8dc9244c8ab30ad2431462f0ee5c5752a15ea419
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-10-17 22:36:47 -0500

    Update install.js
    added a event handler for handling the installation

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -5,12 +5,23 @@ const butInstall = document.getElementById('buttonInstall');
 window.addEventListener('beforeinstallprompt', (event) => {
 
     window.deferredPrompt = event;                  // Store the triggered events
-
     butInstall.classList.toggle('hidden', false);   // Remove the hidden class from the button.
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    // Check if the deferredPrompt is available
+    if (!promptEvent) {
+        return;
+    }
+     // Show the installation prompt to the user
+    promptEvent.prompt();
+    // Reset the deferredPrompt to null after showing the prompt
+    window.deferredPrompt = null;
+    // Hide the install button by adding the 'hidden' class
+    butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});
