94a43dff9e7b08ef1d14d0f4a9d22aef17b42b2d
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-10-17 22:06:13 -0500

    Update database.js
    added function to add content to the database

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,7 +13,15 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);                        // Create a connection to the database database and version we want to use.
+  const tx = db.transaction('jate', 'readwrite');            // Create a new transaction and specify the database and data privileges.
+  const store = tx.objectStore('jate');                      // Open up the desired object store.
+  const request = store.put({ id: 1, value:content });       // use the put method on an object store, where we can store and retrieve data.
+  const result = await request;                              // Get confirmation of the request.
+  console.log('Data saved to the database', result);
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
